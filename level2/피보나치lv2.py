84b56ad650fa6eed0e213545f16fe2b24d521421
yuhee97 <76423543+yuhee97@users.noreply.github.com>
2021-03-09 14:39:32 +0900

    Add files via upload

---
+++ level2/피보나치lv2.py
@@ -0,0 +1,12 @@
+def solution(n):
+    dp = [0] * (n+1)
+    dp[1] = 1; dp[2] = 1
+    
+    for i in range(3, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+        dp[i] %= 1234567
+        
+    return dp[n]
+
+n = 10
+print(solution(n))
