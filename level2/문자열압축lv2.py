c273d89b9f7f6c0b0e71cea541d37e177ade8218
yuhee97 <76423543+yuhee97@users.noreply.github.com>
2021-04-03 16:25:05 +0900

    Add files via upload

---
+++ level2/문자열압축lv2.py
@@ -0,0 +1,39 @@
+import math
+
+def solution(s):
+    result = []; n = len(s)
+    if n == 1:
+        return 1
+    for i in range(1, n):
+        strr = ''
+        for j in range(0, n, i):
+            if j == 0:
+                s_c = s[j:j+i]
+                count = 1
+            else:
+                if j+i < n+1:
+                    if s_c == s[j:j+i]:
+                        count += 1
+                    else:
+                        if count < 2:
+                            strr += s_c
+                            count = 1
+                        else:
+                            strr += (str(count) + s_c)
+                            count = 1
+                else:
+                    strr += s[j:]
+                    break
+                s_c = s[j:j+i]
+                    
+        if count == 1:
+            strr += s_c
+        else:
+            strr += (str(count) + s_c)
+        result.append(len(strr))   
+        
+    return min(result)   
+
+
+s = "aabbaccc"
+print(solution(s))
